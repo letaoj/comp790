a08095c298ed459ae9c3ad4f895a341b4882410e
letaoj <letaoj@gmail.com>
2015-04-22 16:20:25 -0400

    Falut Tolerance added in the application

--- src/Assignment2/ListObserver.java
+++
@@ -1,7 +0,0 @@
-package Assignment2;
-
-public interface ListObserver<ElementType> {
-	void elementAdded(int anIndex, ElementType aNewValue);
-	void elementRemoved(int anIndex, ElementType aNewValue);
-
-}
--- src/Assignment2/ReplicatedSimpleList.java
+++
@@ -1,9 +0,0 @@
-package Assignment2;
-
-
-public interface ReplicatedSimpleList<T> extends SimpleList<T> {
-	void replicatedAdd(int index, T newVal);
-	void replicatedRemove(int index, T oldVal);
-	// void addReplicatingObserver(HistoryObserver<ElementType> anObserver) ;
-
-}
